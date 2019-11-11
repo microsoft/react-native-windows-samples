9d551311573a96581497cace22c0450e3587b0bf
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2019-11-11 12:23:28 -0800

    Added C# and C++/WinRT examples

---
+++ samples/NativeModuleSample/cppwinrt/ios/NativeModuleSample.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface NativeModuleSample : NSObject <RCTBridgeModule>
+
+@end
---
+++ samples/NativeModuleSample/cppwinrt/windows/NativeModuleSample/pch.h
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+#include <unknwn.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Microsoft.ReactNative.Bridge.h>
+#include <winrt/Microsoft.ReactNative.h>
