c7e5fa09a7289d7031b59751c6beaf485d5fb00a
Marlene Cota <mcota@microsoft.com>
2019-11-19 20:14:15 -0800

    more cleanup

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,35 +8,20 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: "User1",
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: "/img/undraw_open_source.svg",
-    infoLink: "https://www.facebook.com",
-    pinned: true
-  }
-];
-
 const repoUrl = "https://github.com/microsoft/react-native-windows";
 
 const siteConfig = {
   title: "React Native for Windows",
   tagline: "Build native Windows apps with Javascript and React",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
-  baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  url: "https://facebook.github.io",
+  baseUrl: "/react-native-windows/",
 
   // Used for publishing and more
   projectName: "react-native-windows",
   organizationName: "microsoft",
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+
+  editUrl:
+    "https://github.com/microsoft/react-native-windows-samples/blob/master/docs/",
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
@@ -46,9 +31,6 @@ const siteConfig = {
     // { search: true }, https://community.algolia.com/docsearch/what-is-docsearch.html
     { href: repoUrl, label: "GitHub" }
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   /* path to images for header/footer */
   headerIcon: "img/header_logo.svg",
@@ -71,23 +53,7 @@ const siteConfig = {
     secondaryColor: "gray"
   },
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
-
+  usePrism: ["javascript", "typescript", "js", "jsx", "cpp", "csharp", "json"],
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: "solarized-dark"
@@ -109,19 +75,14 @@ const siteConfig = {
   ogImage: "img/undraw_online.svg",
   twitterImage: "img/undraw_tweetstorm.svg",
 
+  scrollToTop: true,
+  scrollToTopOptions: {
+    zIndex: 100
+  },
+
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
   docsSideNavCollapsible: true
-
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
