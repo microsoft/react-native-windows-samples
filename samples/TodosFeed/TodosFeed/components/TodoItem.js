9e0362b767ba49cb89d1fc0c5cbe95d436a2ded1
Matteo Pagani <matteo.pagani@microsoft.com>
2020-02-20 16:32:24 +0100

    Updated to the functional component

--- samples/TodosFeed/TodosFeed/components/TodoItem.js
+++ samples/TodosFeed/TodosFeed/components/TodoItem.js
@@ -4,12 +4,16 @@ import {
   View,
 } from 'react-native';
 
-const FeedItem = () => {
+const FeedItem = (props) => {
     return (
         <View style={{ paddingBottom: 12}}>
-            <Text style={{ fontSize: 18}}>{this.props.title}</Text>
+            <Text style={{ fontSize: 18}}>{props.title}</Text>
         </View>
     );
+}
+
+FeedItem.defaultProps = {
+    title: "This is a to-do"
 }
 
 export default FeedItem
