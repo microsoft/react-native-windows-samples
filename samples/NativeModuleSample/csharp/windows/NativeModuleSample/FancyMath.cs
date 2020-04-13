0be26dd4e7be28458c877af763d9b7bbb2b8f435
Jon Thysell <jthysell@microsoft.com>
2020-04-13 11:54:54 -0700

    Update Sample Native Modules to RNW 0.61  (#55)
    * Updated docs
    * Updated readme links
    * Updated both C# and C++/WinRT
    
    Key package.json updates:
    
    ```json
    "peerDependencies": {
        "react": "16.9.0",
        "react-native": "^0.61",
        "react-native-windows": "^0.61"
      },
      "devDependencies": {
        "react": "16.9.0",
        "react-native": "^0.61",
        "react-native-windows": "^0.61"
      }
    ```

--- samples/NativeModuleSample/csharp/windows/NativeModuleSample/FancyMath.cs
+++ samples/NativeModuleSample/csharp/windows/NativeModuleSample/FancyMath.cs
@@ -26,7 +26,7 @@ namespace NativeModuleSample
             }
 
             [ReactEvent]
-            public ReactEvent<double> AddEvent { get; set; }
+            public Action<double> AddEvent { get; set; }
         }
     }
 }
