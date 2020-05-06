8e827b4936a5a62a992aa2d0f20ac46fcee6dc92
user <user@users-Mac-8.homenet.telecomitalia.it>
2020-05-06 16:40:01 +0200

    added MacOS support

--- samples/rssreader/react-native.config.js
+++ samples/rssreader/react-native.config.js
@@ -3,3 +3,12 @@ const path = require('path');
 module.exports = {
   reactNativePath:  fs.realpathSync(path.resolve(require.resolve('react-native-windows/package.json'), '..')),
 };
+const macSwitch = '--use-react-native-macos';
+
+if (process.argv.includes(macSwitch)) {
+  process.argv = process.argv.filter(arg => arg !== macSwitch);
+  process.argv.push('--config=metro.config.macos.js');
+  module.exports = {
+    reactNativePath: 'node_modules/react-native-macos',
+  };
+}
