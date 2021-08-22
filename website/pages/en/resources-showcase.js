36ebf314b3927b8dd098d60a94ef4e8cfd94e68d
Alexander Sklar <asklar@microsoft.com>
2021-08-22 09:25:39 -0700

    .

--- website/pages/en/resources-showcase.js
+++ website/pages/en/resources-showcase.js
@@ -26,61 +26,109 @@ function getBackgroundForCategory(category) {
   else return 'orange';
 }
 
-function renderImg(img) {
-  return <img src={img} height={120} style={{ margin: 10 }}/>
+function renderImg(img, isSmall) {
+  return <img src={img}
+    style={{
+      maxHeight: isSmall ? 40 : 500,
+      minHeight: isSmall ? 40 : 220,
+
+      verticalAlign: 'end',
+    }} />;
 }
-function renderImgs(imgs) {
+function renderImgs(imgs, isSmall) {
   if (!Array.isArray(imgs)) {
     imgs = [imgs];
   }
-  return <td style={{borderColor: 'transparent', padding: 0 }} width={140 * imgs.length} >
-    {imgs.map(renderImg)}
+  const size = isSmall ? 40 : 220;
+  return <td style={{ borderColor: 'transparent', padding: 0, height: size, width: size * imgs.length }} >
+    {imgs.map(i => renderImg(i, isSmall))}
   </td>;
 }
+
+function renderMoreAppImg(a) {
+  return <td style={{ borderColor: 'transparent', margin: 10, width: 240 }}>
+    <div align='center'>{renderImg(a.img, true)}</div>
+  </td>;
+}
+
+function renderMoreAppContent(a) {
+  return <td style={{ borderColor: 'transparent', textAlign: 'center' }}>
+    {renderContent(a)}
+  </td>
+}
+
+function renderMore(a) {
+  return <div style={{}}>
+    …and more!
+    <div align='center'>
+      <table style={{ borderColor: 'transparent', maxWidth: a.entries.length * 240 }}>
+        <tr style={{ maxHeight: 40 }}>
+          {a.entries.map(renderMoreAppImg)}
+        </tr>
+        <tr style={{ height: 12, padding: 0, background: 'white' }}>
+          {a.entries.map(renderMoreAppContent)}
+        </tr>
+      </table>
+    </div>
+  </div>;
+}
+
+function renderContent(app) {
+  const pill = app.category ? <span style={{
+    padding: 3,
+    margin: 10,
+    borderRadius: 2000,
+    color: 'white',
+    verticalAlign: 'super',
+    fontSize: '10px',
+    background: getBackgroundForCategory(app.category)
+  }}>
+    {app.category}
+  </span> : '';
+
+  return <div style={{ minHeight: 24, minWidth: 140 }}>
+    <span style={{
+      fontSize: 22,
+      fontWeight: 500,
+      minWidth: 140,
+      width: 140,
+    }}>
+      {app.header}
+    </span>
+    {pill}
+    {renderDescription(app.description)}
+    <p>{app.link ? <a href={app.link}>See more</a> : ''}</p>
+  </div>
+    ;
+}
 function renderShowcaseApp(app, i) {
-  const img = renderImgs(app.img);
-  const content =     <td style={{borderColor: 'transparent'}} width={400}>
-      <span style={{
-        fontSize: 22,
-        fontWeight: 500,
-      }}>
-        {app.header}
-      </span>
-      <span style={{ 
-        padding: 3,
-        margin: 10,
-        borderRadius: 2000, 
-        color: 'white',
-        verticalAlign: 'super',
-        fontSize: '10px',
-        background: getBackgroundForCategory(app.category)}}>
-          {app.category}
-      </span>
-    <p>{app.description}</p>
-    <p><a href={app.link}>See more</a></p>
-    </td>;
+
+  if (app.category === 'more') return renderMore(app);
+
+  const img = app.img ? renderImgs(app.img, false) : renderImgs(app.logo, true);
+  const content = <td style={{ borderColor: 'transparent' }} width={400}>{renderContent(app)}</td>;
 
   const parts = [img, content];
 
-return <div>
-    <table style={{borderColor: 'transparent'}}><tr>
-      {parts[i%2]}
-      {parts[1-(i%2)]}
+  return <div>
+    <table style={{ borderColor: 'transparent', alignSelf: 'center' }}><tr>
+      {parts[i % 2]}
+      {parts[1 - (i % 2)]}
     </tr>
     </table>
-    </div>;
+  </div>;
 }
 
 class Resources extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {selectedResource: 'repos'};
+    this.state = { selectedResource: 'repos' };
 
     // This binding is necessary to make `this` work in the callback
     this.SelectResource = this.SelectResource.bind(this);
   }
 
-  SelectResource (navItemName) {
+  SelectResource(navItemName) {
     if (typeof document !== 'undefined') {
       var i;
       var x = document.getElementsByClassName("resourcesSideNavLink");
@@ -115,44 +163,29 @@ class Resources extends React.Component {
 
     const Showcase = () => (
       <Section background="light">
-        <div className="content">
+        <div className="content" style={{ width: 1900, maxWidth: '90%' }}>
           <h1>Who's using React Native for Windows</h1>
+          <p>React Native for Desktop empowers developers to target a huge community of users beyond mobile.
+            See how Microsoft uses React Native within strategically key experiences like Xbox, Office, and more.
+            Then check out some key success stories from companies using React Native for Desktop to reach even more users.</p>
           {showcaseApps.map(renderShowcaseApp)}
         </div>
       </Section>
     );
 
     return (
-      // <div className="row">
-      //   <div className="resourcesPageSideNav">
-      //     <Section background="tint">
-      //       <div style={{float: "right", marginRight: 180}}>
-      //         <div className="resourcesPageSideNavTitle">Resources</div>
-      //         <div className="resourcesPageSideNavOptions">
-      //           <a href="./resources" className={'resourcesSideNavLink '}>Repos</a>
-      //         </div>
-      //         <div className="resourcesPageSideNavOptions">
-      //           <a href="./resources-news-social"className="resourcesSideNavLink" >News & Social</a>
-      //         </div>
-      //         <div className="resourcesPageSideNavOptions">
-      //           <a href="./resources-videos" className="resourcesSideNavLink" >Videos</a>
-      //         </div>
-      //         <div className="resourcesPageSideNavOptions">
-      //           <a className="resourcesSideNavLink selected" >Showcase</a>
-      //         </div>
-      //       </div>
-      //     </Section>
-      //   </div>
-        // <div className="column">
-          <div className="homepage" style={{marginLeft: 50}}>
-            <div id="newssocial">
-              <Showcase/>
-            </div>
-          </div>
-        // </div>
-      // </div>
+      <div className="homepage" style={{ marginLeft: 10 }}>
+        <Showcase />
+      </div>
     );
   }
 }
 
 module.exports = Resources;
+function renderDescription(desc) {
+  if (!Array.isArray(desc)) {
+    desc = [desc];
+  }
+  return desc.map(d => <p>{d}</p>);
+}
+
