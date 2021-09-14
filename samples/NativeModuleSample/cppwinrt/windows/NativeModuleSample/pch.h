73beb0289ec74251f107ce941f5fa0352f818fcc
Jon Thysell <jthysell@microsoft.com>
2021-09-14 08:42:53 -0700

    Update NativeModule sample to RNW 0.65
    Updates both the C++ and C# implementations to RNW 0.65.

--- samples/NativeModuleSample/cppwinrt/windows/NativeModuleSample/pch.h
+++ samples/NativeModuleSample/cppwinrt/windows/NativeModuleSample/pch.h
@@ -9,16 +9,8 @@
 #include <restrictederrorinfo.h>
 #include <unknwn.h>
 #include <windows.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Foundation.h>
+#include <CppWinRTIncludes.h>
 #include <winrt/Windows.System.Threading.h>
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
-#include <winrt/Windows.UI.Xaml.Controls.h>
-#include <winrt/Windows.UI.Xaml.Data.h>
-#include <winrt/Windows.UI.Xaml.Interop.h>
-#include <winrt/Windows.UI.Xaml.Markup.h>
-#include <winrt/Windows.UI.Xaml.Navigation.h>
-#include <winrt/Windows.UI.Xaml.h>
 #include <winrt/Windows.Web.Http.h>
 #include <winrt/Windows.Web.Http.Headers.h>
 
