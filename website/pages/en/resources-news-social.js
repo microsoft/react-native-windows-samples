cd807f3d7a66f0b21ce0fcae27fb43f15e3c0a92
Alexander Sklar <asklar@microsoft.com>
2021-10-24 07:06:55 -0700

    add Discord link

--- website/pages/en/resources-news-social.js
+++ website/pages/en/resources-news-social.js
@@ -97,12 +97,19 @@ class Resources extends React.Component {
 
           <a href="https://twitter.com/ReactNativeMSFT?ref_src=twsrc%5Etfw">
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#1d9bf0" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" focusable="false" class="u01b__icon-home">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#1d9bf0" width="32" height="32" viewBox="0 0 24 24" aria-hidden="true" focusable="false" class="u01b__icon-home">
               <path opacity="0" d="M0 0h24v24H0z"></path>
               <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
             </svg>
-            <span style={{verticalAlign: 'top', margin: 5}}> Follow <b>@ReactNativeMSFT</b> on Twitter</span></a>
+            <span style={{ verticalAlign: 'top', margin: 5 }}> Follow <b>@ReactNativeMSFT</b> on Twitter</span></a>
 
+
+          <span><img src="./img/discord.png" style={{ verticalAlign: 'top', width: 32, marginRight: 8 }} />
+            <a href="https://discord.gg/jdhZ4EbhHa">Join the &nbsp;
+              <span style={{ fontFamily: 'monospace', textDecorationLine: 'underline'  }}>#react-native</span>
+              &nbsp; channel on the <b>UWP Community</b> Discord server
+            </a>
+          </span>
         </div>
       </Section>
     );
