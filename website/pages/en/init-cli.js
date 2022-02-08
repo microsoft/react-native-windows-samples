ad3b576e7fb4c5a22ff597b1dc5b581322f2a6d4
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2022-02-08 15:19:57 -0800

    Address Feedback

--- website/pages/en/init-cli.js
+++ website/pages/en/init-cli.js
@@ -100,7 +100,20 @@ class InitCli extends React.Component {
         <p>
           This sends telemetry to Microsoft by default. You can prevent the
           telemetry from being sent by using the <code>--no-telemetry</code> command line
-          option. See the <code>react-native-windows-init</code> README for more details.
+          option. See below for more details.
+        </p>
+        <p>
+        The software may collect information about you and your use of the software and send 
+        it to Microsoft. Microsoft may use this information to provide services and improve 
+        our products and services. You may turn off the telemetry as described in the repository. 
+        There are also some features in the software that may enable you and Microsoft to collect 
+        data from users of your applications. If you use these features, you must comply with 
+        applicable law, including providing appropriate notices to users of your applications 
+        together with a copy of Microsoft's privacy statement. Our privacy statement is located
+        at https://go.microsoft.com/fwlink/?LinkID=824704. You can learn more about data collection
+        and use in the help documentation and our privacy statement. Your use of the software operates
+        as your consent to these practices. This data collection notice only applies to the process 
+        of creating a new React Native for Windows app with the CLI.
         </p>
         </div>
       </Section>
