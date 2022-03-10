d737b30570eaa8b4f760ca33dbe185b52f623a4b
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2022-03-10 12:36:45 -0800

    fix unnecessary change

--- samples/AppServiceDemo/windows/ReactNativeAppServiceModule/Properties/AssemblyInfo.cs
+++ samples/AppServiceDemo/windows/ReactNativeAppServiceModule/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Reactnativeappservicemodule")]
+[assembly: AssemblyTitle("ReactNativeAppServiceModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Reactnativeappservicemodule")]
+[assembly: AssemblyProduct("ReactNativeAppServiceModule")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
