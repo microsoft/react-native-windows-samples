acdb3d6d2ba7c545384a627edfa6a1544e6e3fb9
Alexander Sklar <asklar@microsoft.com>
2022-07-31 13:38:33 -0700

    Make project work in Release, align vcxproj to UWP template

--- samples/CalculatorCoreAppNuGet/windows/CalculatorCoreAppNuGet/App.cpp
+++ samples/CalculatorCoreAppNuGet/windows/CalculatorCoreAppNuGet/App.cpp
@@ -4,5 +4,9 @@
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
+#ifdef _DEBUG
   RNCoreAppStartFromConfigJson(L"app.config.json", nullptr, nullptr);
+#else
+  RNCoreAppStartFromConfigJson(L"app.config.release.json", nullptr, nullptr);
+#endif
 }
