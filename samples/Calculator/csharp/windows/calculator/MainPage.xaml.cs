08493edacfb0d0bce67b0be1e55439e5baefda1a
Yajur Grover <30995198+YajurG@users.noreply.github.com>
2023-12-20 11:20:48 -0800

    Upgrade samples to 0.73 (#906)
    ## Description
    Upgrade the samples to 0.73 as part of 0.73 release
    
    ### Why
    Part of https://github.com/microsoft/react-native-windows/issues/12198
    
    ## Screenshots
    N/A
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/microsoft/react-native-windows-samples/pull/906)

--- samples/Calculator/csharp/windows/calculator/MainPage.xaml.cs
+++ samples/Calculator/csharp/windows/calculator/MainPage.xaml.cs
@@ -17,7 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace calculator
+namespace Calculator
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
