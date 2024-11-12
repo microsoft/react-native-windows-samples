3a51e74aa41ff10c297333ef963b65b8952e55d3
Jon Thysell <jthysell@microsoft.com>
2024-11-12 14:48:23 -0800

    Add example-old windows impl

---
+++ samples/NativeModuleSample/cpp-lib/example-old/windows/NativeModuleSampleExampleOld/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::NativeModuleSampleExampleOld::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::NativeModuleSampleExampleOld::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
