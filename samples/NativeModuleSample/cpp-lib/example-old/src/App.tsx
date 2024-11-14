9f57d2dc913e806116d933c5dccb54516579a9a0
Jon Thysell <jthysell@microsoft.com>
2024-11-13 16:35:36 -0800

    Add proper exports and update example apps

--- samples/NativeModuleSample/cpp-lib/example-old/src/App.tsx
+++ samples/NativeModuleSample/cpp-lib/example-old/src/App.tsx
@@ -1,17 +1,121 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'native-module-sample';
+import {
+  FancyMath,
+  type Response,
+  SimpleHttpModule,
+  DataMarshallingExamples,
+} from 'native-module-sample';
+
+/* eslint-disable react-native/no-inline-styles */
 
 export default function App() {
-  const [result, setResult] = useState<number | undefined>();
+  const [fancyMathAddResult, setFancyMathAddResult] = useState<
+    number | undefined
+  >();
+  const [simpleHttpModuleResponse, setSimpleHttpModuleResponse] = useState<
+    Response | undefined
+  >();
 
   useEffect(() => {
-    multiply(3, 7).then(setResult);
+    FancyMath.add(3, 7, setFancyMathAddResult);
+    SimpleHttpModule.GetHttpResponse('http://example.com').then(
+      setSimpleHttpModuleResponse
+    );
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={{ fontWeight: 'bold' }}>FancyMath:</Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>E:</Text> {FancyMath.E}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>Pi:</Text> {FancyMath.Pi}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>add(3, 7):</Text>{' '}
+        {fancyMathAddResult}
+      </Text>
+      <Text />
+      <Text style={{ fontWeight: 'bold' }}>SimpleHttpModule:</Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          GetHttpResponse("http://example.com"):
+        </Text>{' '}
+        {simpleHttpModuleResponse?.statusCode ?? '???'}
+      </Text>
+      <Text />
+      <Text style={{ fontWeight: 'bold' }}>DataMarshallingExamples:</Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          ReturnExplicitBooleanSync():
+        </Text>{' '}
+        {DataMarshallingExamples.ReturnExplicitBooleanSync().toString()}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          ReturnExplicitIntegerSync():
+        </Text>{' '}
+        {DataMarshallingExamples.ReturnExplicitIntegerSync()}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>ReturnExplicitDoubleSync():</Text>{' '}
+        {DataMarshallingExamples.ReturnExplicitDoubleSync()}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>ReturnExplicitStringSync():</Text>{' '}
+        {DataMarshallingExamples.ReturnExplicitStringSync()}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          GetMidpointSync({'{X: 0, Y: 0}, {X: 1, Y: 1}'}):
+        </Text>{' '}
+        {JSON.stringify(
+          DataMarshallingExamples.GetMidpointSync(
+            { X: 0, Y: 0 },
+            { X: 1, Y: 1 }
+          )
+        )}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          GetLengthSync({'{Start: { X: 0, Y: 0 }, End: { X: 1, Y: 1 }}'}):
+        </Text>{' '}
+        {DataMarshallingExamples.GetLengthSync({
+          Start: { X: 0, Y: 0 },
+          End: { X: 1, Y: 1 },
+        })}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          GetAverageSync([1,2,3,4,5,6]):
+        </Text>{' '}
+        {DataMarshallingExamples.GetAverageSync([1, 2, 3, 4, 5, 6])}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          ConcatenateSync(['hello', 'world']):
+        </Text>{' '}
+        {DataMarshallingExamples.ConcatenateSync(['hello', 'world'])}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          SplitSync('hello world', ' '):
+        </Text>{' '}
+        {JSON.stringify(DataMarshallingExamples.SplitSync('hello world', ' '))}
+      </Text>
+      <Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          GetMidpointByJSValueSync({'{x: 0, y: 0}, {x: 1, y: 1}'}):
+        </Text>{' '}
+        {JSON.stringify(
+          DataMarshallingExamples.GetMidpointByJSValueSync(
+            { x: 0, y: 0 },
+            { x: 1, y: 1 }
+          )
+        )}
+      </Text>
     </View>
   );
 }
--- samples/NativeModuleSample/cpp-lib/src/index.tsx
+++ samples/NativeModuleSample/cpp-lib/src/index.tsx
@@ -1,61 +1,16 @@
-import { NativeModules, Platform } from 'react-native';
+import NativeFancyMath from './NativeFancyMath';
+import NativeDataMarshallingExamples from './NativeDataMarshallingExamples';
+import NativeSimpleHttpModule from './NativeSimpleHttpModule';
 
-const LINKING_ERROR =
-  `The package 'native-module-sample' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+export const FancyMath = {
+  ...NativeFancyMath.getConstants(),
+  add: NativeFancyMath.add,
+};
 
-// @ts-expect-error
-const isTurboModuleEnabled = global.__turboModuleProxy != null;
+export { type Point, type Line } from './NativeDataMarshallingExamples';
+export const DataMarshallingExamples = NativeDataMarshallingExamples;
 
-const NativeFancyMathModule = isTurboModuleEnabled
-  ? require('./NativeFancyMath').default
-  : NativeModules.FancyMath;
-
-const NativeFancyMath = NativeFancyMathModule
-  ? NativeFancyMathModule
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-const NativeDataMarshallingExamplesModule = isTurboModuleEnabled
-  ? require('./NativeDataMarshallingExamples').default
-  : NativeModules.DataMarshallingExamples;
-
-const NativeDataMarshallingExamples = NativeDataMarshallingExamplesModule
-  ? NativeDataMarshallingExamplesModule
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-const NativeSimpleHttpModuleModule = isTurboModuleEnabled
-  ? require('./NativeSimpleHttpModule').default
-  : NativeModules.SimpleHttpModule;
-
-const NativeSimpleHttpModule = NativeSimpleHttpModuleModule
-  ? NativeSimpleHttpModuleModule
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export default {
-  FancyMath: NativeFancyMath,
-  DataMarshallingExamples: NativeDataMarshallingExamples,
-  SimpleHttpModule: NativeSimpleHttpModule,
+export { type Response } from './NativeSimpleHttpModule';
+export const SimpleHttpModule = {
+  GetHttpResponse: NativeSimpleHttpModule.GetHttpResponse,
 };
