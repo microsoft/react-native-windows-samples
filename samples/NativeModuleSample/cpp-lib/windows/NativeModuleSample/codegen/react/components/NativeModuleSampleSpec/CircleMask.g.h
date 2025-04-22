0b39d2dba46477462e9ffbb9c324b346501bdfec
Jon Thysell <jthysell@microsoft.com>
2025-04-22 11:55:49 -0700

    Upgrade NativeModuleSample/cpp-lib to RNW 0.78.4 (#1027)
    ## Description
    
    Upgrade `NativeModuleSample/cpp-lib` to RNW 0.78.4
    
    ### Why
    The example apps crash when using RNW 0.78.2.
    
    ## Screenshots
    N/A

--- samples/NativeModuleSample/cpp-lib/windows/NativeModuleSample/codegen/react/components/NativeModuleSampleSpec/CircleMask.g.h
+++ samples/NativeModuleSample/cpp-lib/windows/NativeModuleSample/codegen/react/components/NativeModuleSampleSpec/CircleMask.g.h
@@ -150,7 +150,7 @@ void RegisterCircleMaskNativeComponent(
           builder.SetUpdateStateHandler([](const winrt::Microsoft::ReactNative::ComponentView &view,
                                      const winrt::Microsoft::ReactNative::IComponentState &newState) noexcept {
             auto userData = view.UserData().as<TUserData>();
-            userData->member(view, newState);
+            userData->UpdateState(view, newState);
           });
         }
 
