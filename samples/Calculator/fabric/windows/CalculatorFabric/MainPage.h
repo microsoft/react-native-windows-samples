450baba033b9c1f0e9b9e882bee91954f03767a1
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-10-13 09:56:48 +0530

    fabric to paper

---
+++ samples/Calculator/fabric/windows/CalculatorFabric/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::CalculatorFabric::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::CalculatorFabric::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
