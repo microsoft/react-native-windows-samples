14060cb2805d63ffa010823f4351ae283d8bfd1f
Vineeth <66076509+vineethkuttan@users.noreply.github.com>
2025-12-11 21:36:56 +0530

    Remove exclusionList and update blacklistRE syntax

--- samples/NativeModuleSample/cpp-lib/example/metro.config.js
+++ samples/NativeModuleSample/cpp-lib/example/metro.config.js
@@ -2,7 +2,6 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const fs = require('fs');
 const path = require('path');
 const escape = require('escape-string-regexp');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 const pack = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
@@ -28,7 +27,7 @@ const config = {
   // We need to make sure that only one version is loaded for peerDependencies
   // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: exclusionList(
+    blacklistRE: 
       modules.map(
         (m) =>
           new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
@@ -42,7 +41,7 @@ const config = {
         new RegExp(`${rnwPath}/target/.*`),
         /.*\.ProjectImports\.zip/,
       ])
-    ),
+    ,
 
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
